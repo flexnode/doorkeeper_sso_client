a26ba73465f702c1364b14ad6f7f26b5b546ab34
John Wong <john@flexnode.com>
2015-08-19 16:56:29 +0800

    Remove old unused files

--- lib/doorkeeper_sso_client.rb
+++ lib/doorkeeper_sso_client.rb
@@ -3,9 +3,7 @@ require 'doorkeeper_sso_client/mixins'
 require "doorkeeper_sso_client/engine"
 require 'doorkeeper_sso_client/config'
 require 'doorkeeper_sso_client/logging'
-require 'doorkeeper_sso_client/passport_verifier'
 require 'doorkeeper_sso_client/warden/support'
-require 'doorkeeper_sso_client/warden/hooks/after_fetch'
 require 'doorkeeper_sso_client/version'
 require 'omniauth/strategies/doorkeeper_sso'
 
