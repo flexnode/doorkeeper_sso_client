a2ee37d58c32edd2e9a2beaf28b3baeb30b1bbaa
Isaac Chong <isaac@flexnode.com>
2015-11-26 16:29:29 +0800

    Took out updating of Client Passport function from migration file.

--- db/migrate/20151105081646_add_token_to_passport.rb
+++ db/migrate/20151105081646_add_token_to_passport.rb
@@ -5,10 +5,11 @@ class AddTokenToPassport < ActiveRecord::Migration
       t.string  :client_uid, index: true
     end
 
-    DoorkeeperSsoClient::Passport.find_each do |p|
-      p.save
-    end
+    # This should be run in a rake task, not in this migration as it'll take too long on production.
+    # DoorkeeperSsoClient::Passport.find_each do |p|
+    #   p.save
+    # end
+    # change_column_null :sso_client_passports, :mobile_token, :string, false
 
-    change_column_null :sso_client_passports, :mobile_token, :string, false
   end
 end
